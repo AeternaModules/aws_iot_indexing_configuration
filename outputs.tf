aea1cac1fa395f7c7ead2149894e608c64197454
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:45:01 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -8,10 +8,10 @@ output "iot_indexing_configurations_region" {
 }
 output "iot_indexing_configurations_thing_group_indexing_configuration" {
   description = "Map of thing_group_indexing_configuration values across all iot_indexing_configurations, keyed the same as var.iot_indexing_configurations"
-  value       = { for k, v in aws_iot_indexing_configuration.iot_indexing_configurations : k => v.thing_group_indexing_configuration if v.thing_group_indexing_configuration != null && length(v.thing_group_indexing_configuration) > 0 }
+  value       = { for k, v in aws_iot_indexing_configuration.iot_indexing_configurations : k => one(v.thing_group_indexing_configuration) if v.thing_group_indexing_configuration != null && length(v.thing_group_indexing_configuration) > 0 }
 }
 output "iot_indexing_configurations_thing_indexing_configuration" {
   description = "Map of thing_indexing_configuration values across all iot_indexing_configurations, keyed the same as var.iot_indexing_configurations"
-  value       = { for k, v in aws_iot_indexing_configuration.iot_indexing_configurations : k => v.thing_indexing_configuration if v.thing_indexing_configuration != null && length(v.thing_indexing_configuration) > 0 }
+  value       = { for k, v in aws_iot_indexing_configuration.iot_indexing_configurations : k => one(v.thing_indexing_configuration) if v.thing_indexing_configuration != null && length(v.thing_indexing_configuration) > 0 }
 }
 
